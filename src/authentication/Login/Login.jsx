ad72ef688c77a23d74eadeafb3a83b955874f016
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-04-24 23:08:30 +0200

    completes styling for login page

--- src/authentication/Login/Login.jsx
+++ src/authentication/Login/Login.jsx
@@ -1,49 +1,70 @@
 import React from 'react';
-import loginImg from "../../assets/login-img.jpg"
-import googleIcon from "../../assets/flat-color-icons_google.png"
-
+import loginImg from '../../assets/login-img.jpg';
+import googleIcon from '../../assets/flat-color-icons_google.png';
+import styles from './Login.module.scss';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <main>
-            <div className="container">
-                <div className="login-image">
-                    <img src={loginImg} alt="login Image" />
+            <div className={styles.login_container}>
+                <div className={styles.login_image}>
+                    <img src={loginImg} alt='login Image' />
                 </div>
-                <div className="">
-                    <h1 className="login-title">
-                        Welcome back, Cey
-                    </h1>
-                    <p className="signup-title">Welcome back! Please enter your details</p>
-                </div>
-                <div className="form">
-                    <div className="form-body">
-
-                        <div className="email">
-                            <label className="form__label" htmlFor="email">Email </label>
-                            <input type="email" id="email" className="form__input" placeholder="Enter email"/>
+                <div className={styles.login_details}>
+                    <div className={styles.login_details_heading}>
+                        <h1 className={styles.login_title}>Welcome back, Cey</h1>
+                        <p className={styles.login_title}>
+                            Welcome back! Please enter your details
+                        </p>
+                    </div>
+                    <form className={styles.form}>
+                        <div className={styles.form_body}>
+                            <div className={styles.email}>
+                                <label className='form__label' htmlFor='email'>
+                                    Email{' '}
+                                </label>
+                                <input
+                                    type='email'
+                                    id='email'
+                                    className={styles.form__input}
+                                    placeholder='Enter email'
+                                />
+                            </div>
+                            <div className='password'>
+                                <label className='form__label' htmlFor='password'>
+                                    Password{' '}
+                                </label>
+                                <input
+                                    className={styles.form__input}
+                                    type='password'
+                                    id='password'
+                                    placeholder='Enter Password'
+                                />
+                            </div>
+                            <div className={styles.forgot_password}>
+                                <p className=''>Remember for 30</p>
+                                <Link className=''>forget password?</Link>
+                            </div>
                         </div>
-                        <div className="password">
-                            <label className="form__label" htmlFor="password">Password </label>
-                            <input className="form__input" type="password" id="password" placeholder="Enter Password"/>
+                        <div className={styles.btn}>
+                            <button type='submit' className={styles.submit_btn}>
+                                Log in
+                            </button>
+                        </div>
+                        <div className={styles.btn}>
+                            <button type='submit' className={styles.google_btn}>
+                                <img src={googleIcon} alt='google Icon' />
+                                Log in with google
+                            </button>
                         </div>
                         <div>
-                            <p className="">Remember for 30</p>
-                            <p className="">forget password?</p>
+                            <p className={styles.signup}>
+                                Don’t have an account?{' '}
+                                <Link to='/signup'> Sign up for free</Link>
+                            </p>
                         </div>
-
-                    </div>
-                    <div className="submit-btn">
-                        <button type="submit" className="btn">Log in</button>
-                    </div>
-                    <div className="google-btn">
-                        <button type="submit" className="btn">
-                            <img src={googleIcon} alt="google Icon" />
-                            Log in</button>
-                    </div>
-                    <div>
-                        <p className="">Don’t have an account? <a> Sign up for free</a></p>
-                    </div>
+                    </form>
                 </div>
             </div>
         </main>
