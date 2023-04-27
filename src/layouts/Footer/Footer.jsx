b39aa8530c6c0ef4731058cd24645b9c6b4b3fb6
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-04-27 16:21:06 +0200

    completes footer for desktop

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
                 </div>
                 <div className={styles.useful_links}>
                     <div className={styles.footer_links}>Useful Links</div>
-                    <ul>
+                    <ul className={styles.useful_li}>
                         <li>Explore</li>
                         <li>Sign Up</li>
                         <li>Login</li>
@@ -24,16 +24,16 @@ const Footer = () => {
                 </div>
                 <div className={styles.form_section}>
                     <div className={styles.footer_title}>Get in Touch</div>
-                    <form action="/" method="post" className={styles.form}>
-                        <input type="text" name="name" className={styles.name} id="name" placeholder="Enter your Name"/>
-                        <input type="email" name="email" className={styles.email} id="email" placeholder="Enter your Email Address"/>
-                        <textarea type="text" name="message" className={styles.message} id="message"
+                    <form action="/" method="post" >
+                        <input type="text" name="name" className={styles.input} id="name" placeholder="Enter your Name"/>
+                        <input type="email" name="email" className={styles.input} id="email" placeholder="Enter your Email Address"/>
+                        <textarea type="text" name="message" className={styles.input} id="message"
                                   placeholder="Enter your message..."></textarea>
                         <button type="submit" className={styles.submit_btn}>Send Message</button>
                     </form>
                 </div>
             </div>
-            <div className='copy-right'>Copyright &copy; 2023 | Nsebo</div>
+            <div className={styles.copy_right}>Copyright &copy; 2023 | Holidaze, designed and programmed by Nsebo</div>
         </footer>
 
     );
