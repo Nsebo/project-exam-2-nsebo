f8a05ab0288cf844c4f558b1bf0c4483e2030d29
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-04-27 11:11:33 +0200

    header and footer

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
     return (
         <footer>
-            <div className="footer-section">
-                <div className="footer-info">
-                <h1 className='footer-name'>
+            <div className={styles.footer_section}>
+                <div className={styles.footer_info}>
+                <h1 className={styles.footer_name}>
                    Holidaze
                 </h1>
                     <p>Your home away from home <strong>where you stay with comfort</strong>
                        </p>
                 </div>
-                <div className="useful-links">
-                    <div className="footer-title">Useful Links</div>
+                <div className={styles.useful_links}>
+                    <div className={styles.footer_links}>Useful Links</div>
                     <ul>
                         <li>Explore</li>
                         <li>Sign Up</li>
@@ -21,14 +22,14 @@ const Footer = () => {
                         <li>Rooms</li>
                     </ul>
                 </div>
-                <div className="">
-                    <div className="footer-title">Get in Touch</div>
-                    <form action="/" method="post" className="form">
-                        <input type="text" name="name" className="name" id="name" placeholder="Enter your Name"/>
-                        <input type="email" name="email" className="email" id="email" placeholder="Enter your Email Address"/>
-                        <textarea type="text" name="message" className="message" id="message"
+                <div className={styles.form_section}>
+                    <div className={styles.footer_title}>Get in Touch</div>
+                    <form action="/" method="post" className={styles.form}>
+                        <input type="text" name="name" className={styles.name} id="name" placeholder="Enter your Name"/>
+                        <input type="email" name="email" className={styles.email} id="email" placeholder="Enter your Email Address"/>
+                        <textarea type="text" name="message" className={styles.message} id="message"
                                   placeholder="Enter your message..."></textarea>
-                        <button type="submit" className="btn">Send Message</button>
+                        <button type="submit" className={styles.submit_btn}>Send Message</button>
                     </form>
                 </div>
             </div>
--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from "../Footer/Footer.module.scss";
 
 const Header = () => {
     return (
         <div>
-            <header className="header">
-                <h1>Holidaze</h1>
-                <nav>
+            <header className={styles.header_container}>
+                <h1 className={styles.header_name}>Holidaze</h1>
+                <nav className={styles.nav_container}>
                     <ul>
                         <li>Explore</li>
                         <li>Rooms</li>
