0ccd1a6a9a9a108f80f4212894289fa1e7a5c28d
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-04-29 21:55:26 +0200

    header

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import styles from './Header.module.scss';
+import Button from '../Button/Button';
 
 const Header = () => {
-    return (
-
-            <header className={styles.header_container}>
-                <div>
-                    <h1 className={styles.header_logo}>Holidaze</h1>
-                </div>
-                <nav className={styles.nav_container}>
-                    <ul className={styles.ul}>
-                        <li>Explore</li>
-                        <li>Rooms</li>
-                        <li>About</li>
-                        <li>List your property</li>
-                    </ul>
-                </nav>
-                <div className={styles.btn}>
-                    <button className={styles.btn_logIn}>Log in</button>
-                    <button className={styles.btn_signUp}>Sign Up</button>
-                </div>
-            </header>
-
-    );
+  return (
+    <header className={styles.header_container}>
+      <div>
+        <h1 className={styles.header_logo}>Holidaze</h1>
+      </div>
+      <nav className={styles.nav_container}>
+        <ul className={styles.ul}>
+          <li>Explore</li>
+          <li>Rooms</li>
+          <li>About</li>
+          <li>List your property</li>
+        </ul>
+      </nav>
+      <div className={styles.btn}>
+        <Button buttonText='Login' to='/login' />
+        <Button buttonText='Sign Up' to='/signup' gray={true} />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
