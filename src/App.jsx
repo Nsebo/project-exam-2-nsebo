910ed7c1ab8ea72bb4b459aa44a7e623611d8996
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-04-29 21:53:04 +0200

    app.css, app.jsx. index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import { useState } from 'react'
-import Signup from "./authentication/Signup/Signup.jsx";
-import Login from "./authentication/Login/Login.jsx";
-import Header from "./layouts/Header/Header.jsx";
-import Hero from "./components/Hero/Hero.jsx";
-import Footer from "./layouts/Footer/Footer.jsx";
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Route, Routes} from "react-router-dom";
+import { useState } from 'react';
+import Signup from './authentication/Signup/Signup.jsx';
+import Login from './authentication/Login/Login.jsx';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="app">
-      <Routes>
-        <Route path="signup" element={<Signup/>}/>
-        <Route path="login" element={<Login/>}/>
-          <Route path="header" element={<Header/>}/>
-          <Route path="hero" element={<Hero/>}/>
-          <Route path="footer" element={<Footer/>}/>
-      </Routes>
-
-    </div>
-  )
+      <div className='app'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='signup' element={<Signup />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
+      </div>
+  );
 }
 
-export default App
+export default App;
