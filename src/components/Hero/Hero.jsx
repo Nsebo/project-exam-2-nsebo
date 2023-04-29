acccc2d95d643e66fa017bf662e013757b0c66bb
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-04-29 22:00:58 +0200

    hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,15 +4,15 @@ import styles from './Hero.module.scss';
 import Button from '../../layouts/Button/Button';
 
 const Hero = () => {
-  return (
-    <section className={styles.hero_container}>
-      <div className={styles.hero_details}>
-        <h1>STAY WITH COMFORT</h1>
-        <p>Your home away from home where you stay with comfort</p>
-        <Button buttonText='Book Now' to='/book-now' />
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.hero_container}>
+            <div className={styles.hero_details}>
+                <h1>STAY WITH COMFORT</h1>
+                <p>Your home away from home where you stay with comfort</p>
+                <Button buttonText='Book Now' to='/book-now' />
+            </div>
+        </section>
+    );
 };
 
 export default Hero;
