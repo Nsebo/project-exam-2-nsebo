2dde106547bc8dd1463271647f2fdc1b7d7a59c6
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-12 20:26:44 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,29 @@
 import styles from './Home.module.scss';
 import Layout from '../../layouts/Layout/Layout';
 import Hero from '../../components/Hero/Hero';
+import Location from '../../components/Location/Location';
+import VenueCard from '../../components/VenueCard/VenueCard';
+import Subscribe from '../../components/Subscribe/Subscribe';
+import Venue from '../../components/Venue/Venue';
+import { VenueContext } from '../../context/venue-context';
+import { useContext } from 'react';
+import Pictures from '../../components/Pictures/Pictures';
 
 const Home = () => {
-  return (
-    <div className={styles.home}>
-      <Layout>
-        <Hero />
-      </Layout>
-    </div>
-  );
+    const { venues } = useContext(VenueContext);
+
+    return (
+        <div className={styles.home}>
+            <Layout>
+                <Hero />
+                <Location />
+                {/* <VenueCard /> */}
+                {venues && <Venue />}
+                {venues && <Pictures />}
+                <Subscribe />
+            </Layout>
+        </div>
+    );
 };
 
 export default Home;
