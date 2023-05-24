87523c2eee600e90bb823fb3d03ce5552bbc49e4
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-24 14:12:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { useState } from 'react';
 import Signup from './authentication/Signup/Signup.jsx';
 import Login from './authentication/Login/Login.jsx';
-import Profile from "./pages/profile/Profile.jsx";
-import viteLogo from '/vite.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
+import Venues from './pages/Venues/Venues.jsx';
+import Venue from './pages/Venue/Venue.jsx';
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div className='app'>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='signup' element={<Signup />} />
-                <Route path='login' element={<Login />} />
-                <Route path='profile' element={<Profile />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='login' element={<Login />} />
+        <Route path='/venues' element={<Venues />} />
+        <Route path='/venue/:venueId' element={<Venue />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
