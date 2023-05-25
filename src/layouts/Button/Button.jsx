7426cf7e7b889c9f0974cc66695f7c8432bad829
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-25 12:22:00 +0200

    updates all the buttons

--- src/layouts/Button/Button.jsx
+++ src/layouts/Button/Button.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
 
-const Button = ({ buttonText, to, gray, border }) => {
+const Button = ({ buttonText, to, gray,  white, border }) => {
   return (
     <Link
       to={to}
-      className={`${styles.button} ${gray && styles.gray} ${
+      className={`${styles.button} ${gray && styles.gray}  ${white && styles.white} ${
         border && styles.border
       }`}
     >
