c1b219ac1bd8af56e78f42d7ae85f010dbb1d132
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-25 12:21:02 +0200

    cleans up code

--- src/components/Venue/Venue.jsx
+++ src/components/Venue/Venue.jsx
@@ -2,19 +2,27 @@ import React, { useContext } from 'react';
 import styles from './Venue.module.scss';
 import { VenueContext } from '../../context/venue-context';
 import VenueCard from '../VenueCard/VenueCard';
+import { Link } from 'react-router-dom';
 
 const Venue = () => {
   const { venues } = useContext(VenueContext);
-  const sixVenues = venues.slice(0, 6);
+  const sixVenues = venues.slice(0, 3);
 
   return (
     <section className={styles.venue_wrapper}>
-      <h3>TOP DESTINATION</h3>
-      <h2>Explore Top Destination</h2>
+   <div className={styles.venue_details}>
+       <div>
+           <h2>Explore our featured venues</h2>
+           <p>Featured Venues</p>
+       </div>
+       <Link to="/venues">View all items</Link>
+   </div>
+
       <div className={styles.venue_container}>
-        {sixVenues.map((venue) => {
+        {sixVenues.map((venue, idx) => {
           return (
             <VenueCard
+                key={idx}
               imgUrl={venue.media[0]}
               price={venue.price}
               name={venue.name}
