cdad0e75044c995851b3f85aa848c3b0af820a6f
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-26 13:53:21 +0200

    changes the hero paragraph

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,8 @@ const Hero = () => {
     <section className={styles.hero_container}>
       <div className={styles.hero_details}>
         <h1>STAY WITH COMFORT</h1>
-        <p>Your home away from home where you stay with comfort</p>
+        <p>Stay in the world's most remarkable homes
+        </p>
         <Button buttonText='Book Now' to='/book-now' white={true}/>
       </div>
     </section>
