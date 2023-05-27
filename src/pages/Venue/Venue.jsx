519d1d9e3e40fdc42c73c6f8bf030b71d5e59a1d
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-27 20:34:36 +0200

    Update Venue.jsx

--- src/pages/Venue/Venue.jsx
+++ src/pages/Venue/Venue.jsx
@@ -40,7 +40,6 @@ const Venue = () => {
   // console.log(formattedUpdatedDate);
 
   const { venueId } = useParams();
-
   useEffect(() => {
     axios
       .get(`https://nf-api.onrender.com/api/v1/holidaze/venues/${venueId}`)
@@ -53,7 +52,7 @@ const Venue = () => {
   }, []);
   return (
     <Layout>
-      <div className={styles.container}>
+      <div className={styles.venue_container}>
         <div className={styles.venue_details}>
           <h3>{name}</h3>
           <div className={styles.rate}>
@@ -69,73 +68,82 @@ const Venue = () => {
           </div>
         </div>
         <div className={styles.images}>
-          <img src={media} alt='venue image' />
-          <img src={media} alt='venue image' />
-          <img src={media} alt='venue image' />
+          <div className=''>
+            <img src={media} alt='venue image' className={styles.first_image} />
+          </div>
+          <div className={styles.second}>
+            <img
+              src={media}
+              alt='venue image'
+              className={styles.second_image}
+            />
+            <img src={media} alt='venue image' className={styles.third_image} />
+          </div>
         </div>
 
-        <div className={styles.hosted}>
-          <h4>Entire villa hosted by Cey</h4>
-          <p>{maxGuests} guests</p>
-          {/* <small>{formattedCreatedDate}</small>
-          <small>{formattedUpdatedDate}</small> */}
-        </div>
-        <>
-          <h2>Enjoy our amenities</h2>
-          <div className={styles.amenities}>
-            <div className={styles.wifi}>
-              <img src={wifi} alt='wifi' />
-              <p>Wifi</p>
+        <div className={styles.bottom}>
+          <div>
+            <div className={styles.hosted}>
+              <h4>Entire villa hosted by Cey</h4>
+              <p>{maxGuests} guests</p>
+              {/* <small>{formattedCreatedDate ? formattedCreatedDate : ''}</small>
+          <small>{formattedUpdatedDate ? formattedUpdatedDate : ''}</small> */}
             </div>
-            <div className={styles.parking}>
-              <img src={car} alt='parking' />
-              <p>Parking</p>
+            <h2>Enjoy our amenities</h2>
+            <div className={styles.amenities}>
+              <div className={styles.wifi}>
+                <img src={wifi} alt='wifi' />
+                <p>Wifi</p>
+              </div>
+              <div className={styles.parking}>
+                <img src={car} alt='parking' />
+                <p>Parking</p>
+              </div>
+              <div className={styles.breakfast}>
+                <img src={breakfast} alt='breakfast' />
+                <p>BreakFast</p>
+              </div>
+              <div className={styles.pets}>
+                <img src={pets} alt='pets' />
+                <p>Pets</p>
+              </div>
             </div>
-            <div className={styles.breakfast}>
-              <img src={breakfast} alt='breakfast' />
-              <p>BreakFast</p>
-            </div>
-            <div className={styles.pets}>
-              <img src={pets} alt='pets' />
-              <p>Pets</p>
+            <div>
+              <h2>Where will you be</h2>
+              <img src={map} alt='map' />
             </div>
           </div>
-          <>
-            <h2>Where will you be</h2>
-            <img src={map} alt='map' />
-          </>
-        </>
-        <div className={styles.bookings}>
-          <h3>{price}</h3>
-          <>
-            <div className={styles.checks}>
-              <label htmlFor='checkIn'>
-                {' '}
-                Check-In
-                <input type='date' />
-              </label>
-              <label htmlFor='checkOut'>
-                {' '}
-                Check-Out
-                <input type='date' />
-              </label>
+          <div className={styles.bookings}>
+            <h3>{price}kr NOK nights</h3>
+            <div className={styles.bookings_details}>
+              <div className={styles.checks}>
+                <label htmlFor='checkIn'>
+                  {' '}
+                  Check-In
+                  <input type='date' />
+                </label>
+                <label htmlFor='checkOut'>
+                  {' '}
+                  Check-Out
+                  <input type='date' />
+                </label>
+              </div>
+              <div className={styles.guests}>
+                <label htmlFor='guests'>
+                  Guests
+                  <select name='guest'>
+                    <option value='one'>1 guest</option>
+                    <option value='two'>2 guest</option>
+                    <option value='three'>3 guest</option>
+                    <option value='four'>4 guest</option>
+                    <option value='five'>5 guest</option>
+                  </select>
+                </label>
+              </div>
+              <Button buttonText='Reserve' gray={true} width={true} />
+              <small>You won't be charged yet</small>
             </div>
-            <div className={styles.guests}>
-              <label htmlFor='guests'>
-                {' '}
-                Guests
-                <select name='guest'>
-                  <option value='one'>1 guest</option>
-                  <option value='two'>2 guest</option>
-                  <option value='three'>3 guest</option>
-                  <option value='four'>4 guest</option>
-                  <option value='five'>5 guest</option>
-                </select>
-              </label>
-            </div>
-            <Button buttonText='Reserve' gray={true} />
-            <small>You won't be charged yet</small>
-          </>
+          </div>
         </div>
       </div>
     </Layout>
