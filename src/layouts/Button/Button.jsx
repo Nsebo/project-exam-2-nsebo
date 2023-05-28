2f239897e763a7af1e3f977686add9adfedc4fd8
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-28 11:57:48 +0200

    more styling on the button

--- src/layouts/Button/Button.jsx
+++ src/layouts/Button/Button.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
 
-const Button = ({ buttonText, to, gray,  white, border }) => {
+const Button = ({ buttonText, to, gray,  white, border, width }) => {
   return (
     <Link
       to={to}
       className={`${styles.button} ${gray && styles.gray}  ${white && styles.white} ${
+          width && styles.width
+      } ${
         border && styles.border
       }`}
     >
