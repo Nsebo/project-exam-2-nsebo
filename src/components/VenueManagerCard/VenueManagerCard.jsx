f32b601309afff9a667732f7ad16fc83697e74e1
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-30 20:15:42 +0200

    Update VenueManagerCard.jsx

--- src/components/VenueManagerCard/VenueManagerCard.jsx
+++ src/components/VenueManagerCard/VenueManagerCard.jsx
@@ -1,23 +1,62 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './VenueManagerCard.module.scss';
-import Button from '../../layouts/Button/Button';
+import axios from 'axios';
+import { useState } from 'react';
 
-const VenueManagerCard = () => {
-  return (
-    <div>
-      <img src='' alt='' />
-      <div className={styles.name}>
-        <h2>Name</h2>
-        <Link>View Venue</Link>
-      </div>
-      <p>Description</p>
-      <p>Price</p>
-      <div className={styles.but}>
-        <Button buttonText='Edit' />
-        <Button buttonText='Delete' />
-      </div>
-    </div>
-  );
+const VenueManagerCard = ({
+                              name,
+                              id,
+                              description,
+                              price,
+                              media,
+                              onDelete,
+                          }) => {
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleDelete = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+
+            await axios.delete(
+                `https://nf-api.onrender.com/api/v1/holidaze/venues/${id}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                }
+            );
+
+            // Venue delete successful
+            // You can redirect the user or show a success message here
+            alert('Venue delete successful');
+            onDelete(id); // Call the onDelete callback from the parent component
+        } catch (error) {
+            // Handle any network errors or server errors
+            setError('Error: ' + error.response.data.message);
+        }
+    };
+
+    console.log(error);
+    return (
+        <div className={styles.venueManagerCard_container}>
+            <img src={media} alt='venue image' />
+            <div className={styles.des}>
+                <div className={styles.name}>
+                    <h2>{name}</h2>
+                    <Link to={`/venue/${id}`}>View Venue</Link>
+                </div>
+                <p>{description}</p>
+                <p>{price}kr</p>
+                <div className={styles.but}>
+                    <button className={styles.edit}>Edit</button>
+                    <button className={styles.delete} onClick={handleDelete}>
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default VenueManagerCard;
