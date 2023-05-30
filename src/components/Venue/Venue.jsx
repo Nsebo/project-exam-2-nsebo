22d743f341af690b5d0d9920959d571ed84057a8
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-30 14:46:27 +0200

    completes venue pages

--- src/components/Venue/Venue.jsx
+++ src/components/Venue/Venue.jsx
@@ -2,38 +2,29 @@ import React, { useContext } from 'react';
 import styles from './Venue.module.scss';
 import { VenueContext } from '../../context/venue-context';
 import VenueCard from '../VenueCard/VenueCard';
-import { Link } from 'react-router-dom';
 
 const Venue = () => {
-  const { venues } = useContext(VenueContext);
-  const sixVenues = venues.slice(0, 3);
+    const { venues } = useContext(VenueContext);
+    const sixVenues = venues.slice(0, 6);
 
-  return (
-    <section className={styles.venue_wrapper}>
-   <div className={styles.venue_details}>
-       <div>
-           <h2>Explore our featured venues</h2>
-           <p>Featured Venues</p>
-       </div>
-       <Link to="/venues">View all items</Link>
-   </div>
-
-      <div className={styles.venue_container}>
-        {sixVenues.map((venue, idx) => {
-          return (
-            <VenueCard
-                key={idx}
-              imgUrl={venue.media[0]}
-              price={venue.price}
-              name={venue.name}
-                id={venue.id}
-
-            />
-          );
-        })}
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.venue_wrapper}>
+            <h3>TOP DESTINATION</h3>
+            <h2>Explore Top Destination</h2>
+            <div className={styles.venue_container}>
+                {sixVenues.map((venue) => {
+                    return (
+                        <VenueCard
+                            imgUrl={venue.media[0]}
+                            price={venue.price}
+                            name={venue.name}
+                            id={venue.id}
+                        />
+                    );
+                })}
+            </div>
+        </section>
+    );
 };
 
 export default Venue;
