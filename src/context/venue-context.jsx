e1583bfc9ac9305873208f6b772caebfe5327ba7
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-30 20:15:53 +0200

    Update venue-context.jsx

--- src/context/venue-context.jsx
+++ src/context/venue-context.jsx
@@ -2,19 +2,26 @@ import axios from 'axios';
 import { createContext, useContext, useEffect, useState } from 'react';
 
 export const VenueContext = createContext(null);
+
 export const VenueProvider = ({ children }) => {
   const [venues, setVenues] = useState('');
+  const [searchVenues, setSearchVenues] = useState('');
+
   useEffect(() => {
     axios
       .get('https://nf-api.onrender.com/api/v1/holidaze/venues')
       .then((res) => {
         setVenues(res.data);
+        setSearchVenues(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
   return (
-    <VenueContext.Provider value={{ venues }}>{children}</VenueContext.Provider>
+    <VenueContext.Provider value={{ venues, setSearchVenues, searchVenues }}>
+      {children}
+    </VenueContext.Provider>
   );
 };
