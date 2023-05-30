a73147ec4af2c3aab68ed33fa8ec996baceb3216
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-30 14:43:32 +0200

    Update Location.jsx

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,62 +1,23 @@
 import styles from './Location.module.scss';
-import mapIcon from '../../assets/map-pin.svg';
-import calenderIcon from '../../assets/calendar.svg';
-import userIcon from '../../assets/user.svg';
 import searchIcon from '../../assets/search.svg';
 
 const Location = () => {
-  return (
-    <form className={styles.heroBanner}>
-      <div className={styles.tab}>
-        <span className={styles.mapIcon}>
-          <img src={mapIcon} alt='map icon' />
-        </span>
-        <input
-          type='text'
-          name='text'
-          className={styles.name}
-          id='text'
-          placeholder='Where are you going?'
-        />
-      </div>
-      <div className={styles.tab}>
-        <span>
-          <img src={calenderIcon} alt='map icon' />
-        </span>
-        <input
-          type='text'
-          name='text'
-          className={styles.name}
-          id='text'
-          placeholder='Check in date - Check out date'
-        />
-      </div>
-      <div className={styles.tab}>
-        <span>
-          <img src={userIcon} alt='user icon' />
-        </span>
-        <input
-          type='text'
-          name='text'
-          className={styles.name}
-          id='text'
-          placeholder='1 adult. 0 children . 1 room'
-        />
-      </div>
-      <div className={styles.search}>
+    return (
+        <form className={styles.heroBanner}>
+            <div className={styles.search}>
         <span>
           <img src={searchIcon} alt='search icon' />
         </span>
-        <input
-          type='search'
-          name='text'
-          className={styles.search}
-          id='text'
-          placeholder='Search'
-        />
-      </div>
-    </form>
-  );
+                <input
+                    type='search'
+                    name='text'
+                    className={styles.search}
+                    id='text'
+                    placeholder='Search'
+                />
+            </div>
+        </form>
+    );
 };
 
 export default Location;
