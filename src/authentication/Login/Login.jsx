eec0c19c32becac5b40bb6ca01a716f75e8279f9
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-30 13:53:50 +0200

    updates the auth folder

--- src/authentication/Login/Login.jsx
+++ src/authentication/Login/Login.jsx
@@ -1,96 +1,93 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import loginImg from '../../assets/login-img.jpg';
 import googleIcon from '../../assets/flat-color-icons_google.png';
 import styles from './Login.module.scss';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { LoginContext } from '../../context/login-context';
+import { AuthContext } from '../../context/auth-context';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch('https://nf-api.onrender.com/api/v1/holidaze/auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle successful login
-        console.log(data);
-      })
-      .catch((error) => {
-        // Handle login error
-        setError(error.message);
-      });
-  };
+  const {
+    email,
+    password,
+    errorMessage,
+    setEmail,
+    data,
+    setPassword,
+    handleLoginSubmit,
+  } = useContext(LoginContext);
+  const { signUpData } = useContext(AuthContext);
   return (
-    <main>
-      <div className={styles.login_container}>
-        <div className={styles.login_image}>
-          <img src={loginImg} alt='login Image' />
-        </div>
-        <div className={styles.login_details}>
-          <div className={styles.login_details_heading}>
-            <h1 className={styles.login_title}>Welcome back, Cey</h1>
-            <p className={styles.signup_title}>
-              Welcome back! Please enter your details
-            </p>
+      <main>
+        <div className={styles.login_container}>
+          <div className={styles.login_image}>
+            <img src={loginImg} alt='login Image' />
           </div>
-          <form className={styles.form} onSubmit={handleSubmit}>
-            <div className={styles.form_body}>
-              <div className={styles.email}>
-                <label className='form__label' htmlFor='email'>
-                  Email{' '}
-                </label>
-                <input
-                  type='email'
-                  id='email'
-                  className={styles.form__input}
-                  placeholder='Enter email'
-                />
-              </div>
-              <div className='password'>
-                <label className='form__label' htmlFor='password'>
-                  Password{' '}
-                </label>
-                <input
-                  className={styles.form__input}
-                  type='password'
-                  id='password'
-                  placeholder='Enter Password'
-                />
-              </div>
-              <div className={styles.forgot_password}>
-                <p className=''>Remember for 30</p>
-                <Link className=''>forget password?</Link>
-              </div>
-            </div>
-            <div className={styles.btn}>
-              <button type='submit' className={styles.submit_btn}>
-                Log in
-              </button>
-            </div>
-            <div className={styles.btn}>
-              <button type='submit' className={styles.google_btn}>
-                <img src={googleIcon} alt='google Icon' />
-                Log in with google
-              </button>
-            </div>
-            <div>
-              <p className={styles.signup}>
-                Don’t have an account?{' '}
-                <Link to='/signup'> Sign up for free</Link>
+          <div className={styles.login_details}>
+            <div className={styles.login_details_heading}>
+              <h1 className={styles.login_title}>
+                Welcome back, {signUpData.name}
+              </h1>
+              <p className={styles.signup_title}>
+                Welcome back! Please enter your details
               </p>
             </div>
-          </form>
+            <form className={styles.form} onSubmit={handleLoginSubmit}>
+              <div className={styles.form_body}>
+                <div className={styles.email}>
+                  <label className='form__label' htmlFor='email'>
+                    Email{' '}
+                  </label>
+                  <input
+                      type='email'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      className={styles.form__input}
+                      placeholder='Enter email'
+                  />
+                </div>
+                <div className='password'>
+                  <label className='form__label' htmlFor='password'>
+                    Password{' '}
+                  </label>
+                  <input
+                      type='password'
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      className={styles.form__input}
+                      placeholder='Enter Password'
+                      required
+                  />
+                </div>
+                <div className={styles.forgot_password}>
+                  <p className=''>Remember for 30</p>
+                  <Link className=''>forget password?</Link>
+                </div>
+              </div>
+              {errorMessage && <p>{errorMessage}</p>}
+              <div className={styles.btn}>
+                <button type='submit' className={styles.submit_btn}>
+                  Log in
+                </button>
+              </div>
+              <div className={styles.btn}>
+                <button type='submit' className={styles.google_btn}>
+                  <img src={googleIcon} alt='google Icon' />
+                  Log in with google
+                </button>
+              </div>
+              <div>
+                <p className={styles.signup}>
+                  Don’t have an account?{' '}
+                  <Link to='/signup'> Sign up for free</Link>
+                </p>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
   );
 };
 
--- src/authentication/Signup/Signup.jsx
+++ src/authentication/Signup/Signup.jsx
@@ -1,130 +1,146 @@
-
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import signupImg from '../../assets/signup-img.jpg';
 import googleIcon from '../../assets/flat-color-icons_google.png';
 import styles from './Signup.module.scss';
 import { Link } from 'react-router-dom';
-
-
+import axios from 'axios';
+import { AuthContext } from '../../context/auth-context';
 
 const Signup = () => {
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const data = {
-      name: name,
-      email: email,
-      password: password,
-    };
-
-    fetch('https://nf-api.onrender.com/api/v1/holidaze/auth/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
-  };
+  const {
+    name,
+    email,
+    avatar,
+    venueManager,
+    password,
+    confirmPassword,
+    setAvatar,
+    setConfirmPassword,
+    setEmail,
+    setName,
+    setVenueManager,
+    handleSubmit,
+    setPassword,
+    errorMessage,
+    signUpData,
+  } = useContext(AuthContext);
 
   return (
-    <main>
-      <div className={styles.signup_container}>
-        <div className={styles.signup_image}>
-          <img src={signupImg} alt='Signup Image' />
-        </div>
-        <div className={styles.signup_details}>
-          <div className={styles.signup_details_heading}>
-            <h1 className={styles.signup_title}>Create an Account</h1>
-            <p className={styles.signup_para}>
-              Became holidaze member ,you’ll get exlusive offers from holidaze
-            </p>
+      <main>
+        <div className={styles.signup_container}>
+          <div className={styles.signup_image}>
+            <img src={signupImg} alt='Signup Image' />
           </div>
-          <form className={styles.form} onSubmit={handleSubmit}>
-            <div className={styles.form_body}>
-              <div className={styles.username}>
-                <label className='form__label' htmlFor='name'>
-                  Name{''}{' '}
-                </label>
-                <input
-                  className={styles.form_input}
-                  type='text'
-                  id='name'
-                  placeholder='Enter your name'
-                />
-              </div>
-              <div className={styles.email}>
-                <label className={styles.form_label} htmlFor='email'>
-                  Email{''}{' '}
-                </label>
-                <input
-                  type='email'
-                  id='email'
-                  className={styles.form_input}
-                  placeholder='Enter email'
-                />
-              </div>
-              <div className={styles.password}>
-                <label className={styles.form_label} htmlFor='password'>
-                  Password{''}{' '}
-                </label>
-                <input
-                  className={styles.form_input}
-                  type='password'
-                  id='password'
-                  placeholder='Create a Password'
-                />
-              </div>
-              <div className={styles.confirm_password}>
-                <label className={styles.form_label} htmlFor='confirmPassword'>
-                  Confirm Password {''}
-                </label>
-                <input
-                  className={styles.form_input}
-                  type='password'
-                  id='confirmPassword'
-                  placeholder='Confirm Password'
-                />
-              </div>
-              <div className={styles.Avatar}>
-                <label className={styles.form_label} htmlFor='avatar'>
-                  Avatar{''}
-                </label>
-                <input
-                  type='avatar'
-                  name='avatar'
-                  id='avatar'
-                  className={styles.form_input}
-                  placeholder='Avatar url'
-                />
-              </div>
-            </div>
-            <div className={styles.btn}>
-              <button type='submit' className={styles.submit_btn}>
-                Create an Account
-              </button>
-              <button type='submit' className={styles.google_btn}>
-                <img src={googleIcon} alt='google Icon' />
-                Sign up with Google
-              </button>
-            </div>
-
-            <div>
-              <p className={styles.signup}>
-                Already have an account?{''} <Link>log in</Link>
+          <div className={styles.signup_details}>
+            <div className={styles.signup_details_heading}>
+              <h1 className={styles.signup_title}>Create an Account</h1>
+              <p className={styles.signup_para}>
+                Became holidaze member, you’ll get exclusive offers from holidaze
               </p>
             </div>
-          </form>
+            <form className={styles.form} onSubmit={handleSubmit}>
+              <div className={styles.form_body}>
+                <div className={styles.username}>
+                  <label className='form__label' htmlFor='name'>
+                    Name:
+                  </label>
+                  <input
+                      className={styles.form_input}
+                      type='text'
+                      id='name'
+                      placeholder='Enter your name'
+                      name='name'
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className={styles.email}>
+                  <label className={styles.form_label} htmlFor='email'>
+                    Email:
+                  </label>
+                  <input
+                      type='email'
+                      id='email'
+                      className={styles.form_input}
+                      placeholder='Enter email'
+                      name='email'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className={styles.password}>
+                  <label className={styles.form_label} htmlFor='password'>
+                    Password:
+                  </label>
+                  <input
+                      className={styles.form_input}
+                      type='password'
+                      id='password'
+                      placeholder='Create a Password'
+                      name='password'
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className={styles.confirm_password}>
+                  <label className={styles.form_label} htmlFor='confirmPassword'>
+                    Confirm Password:
+                  </label>
+                  <input
+                      className={styles.form_input}
+                      type='password'
+                      id='confirmPassword'
+                      placeholder='Confirm Password'
+                      name='confirmPassword'
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+                <div className={styles.Avatar}>
+                  <label className={styles.form_label} htmlFor='avatar'>
+                    Avatar:
+                  </label>
+                  <input
+                      type='text'
+                      name='avatarUrl'
+                      id='avatar'
+                      className={styles.form_input}
+                      placeholder='Avatar URL'
+                      value={avatar}
+                      onChange={(e) => setAvatar(e.target.value)}
+                  />
+                </div>
+                <div className={styles.venueManager}>
+                  <label className={styles.form_label} htmlFor='venueManager'>
+                    Venue Manager:
+                    <input
+                        type='checkbox'
+                        checked={venueManager}
+                        onChange={(e) => setVenueManager(e.target.checked)}
+                    />
+                  </label>
+                </div>
+              </div>
+              {errorMessage && <p>{errorMessage}</p>}
+              <div className={styles.btn}>
+                <button type='submit' className={styles.submit_btn}>
+                  Create an Account
+                </button>
+                <button type='submit' className={styles.google_btn}>
+                  <img src={googleIcon} alt='Google Icon' />
+                  Sign up with Google
+                </button>
+              </div>
+
+              <div>
+                <p className={styles.signup}>
+                  Already have an account? <Link to='/login'>Log in</Link>
+                </p>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
   );
 };
 
