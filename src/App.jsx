5becc649c5c5f595d691a16084006cbd3b52e7b7
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-30 13:52:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 import Venues from './pages/Venues/Venues.jsx';
 import Venue from './pages/Venue/Venue.jsx';
+import CreateVenue from './components/CreateVenue/CreateVenue.jsx';
+import VenueManager from './pages/VenueManager/VenueManager.jsx';
 
 function App() {
-  return (
-    <div className='app'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='signup' element={<Signup />} />
-        <Route path='login' element={<Login />} />
-        <Route path='/venues' element={<Venues />} />
-        <Route path='/venue/:venueId' element={<Venue />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className='app'>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='signup' element={<Signup />} />
+                <Route path='login' element={<Login />} />
+                <Route path='/venues' element={<Venues />} />
+                <Route path='/my-venues' element={<VenueManager />} />
+                <Route path='/create-venues' element={<CreateVenue />} />
+                <Route path='/venue/:venueId' element={<Venue />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
