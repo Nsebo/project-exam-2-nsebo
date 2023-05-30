ddd016365951736fa56d4b711c89c279fa17e656
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-30 20:13:42 +0200

    Update Header.jsx

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Header.module.scss';
 import Button from '../Button/Button';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/auth-context';
+import { LoginContext } from '../../context/login-context';
 
 const Header = () => {
-  return (
-    <header className={styles.header_container}>
-      <Link to="/">
-        <h1 className={styles.header_logo}>
-           Holidaze
-        </h1>
-      </Link>
-      <nav className={styles.nav_container}>
-        <ul className={styles.ul}>
-          <Link to='/venues'>Venues</Link>
-            <Link>List a room</Link>
-        </ul>
-      </nav>
-      <div className={styles.btn}>
-        <Button buttonText='Login' to='/login' white={true} />
-        <Button buttonText='Sign Up' to='/signup' gray={true} />
-      </div>
-    </header>
-  );
+    const { data } = useContext(LoginContext);
+
+    return (
+        <header className={styles.header_container}>
+            <Link to="/">
+                <h1 className={styles.header_logo}>Holidaze</h1>
+            </Link>
+            <nav className={styles.nav_container}>
+                <ul className={styles.ul}>
+                    <Link to='/venues'>Explore</Link>
+                    <Link to='/my-venues'>My Venues</Link>
+                    <Link to='/create-venues'>List a room</Link>
+                </ul>
+            </nav>
+            {data?.accessToken ? (
+                <p>Hello {data.name}</p>
+            ) : (
+                <div className={styles.btn}>
+                    <Button buttonText='Login' to='/login' />
+                    <Button buttonText='Sign Up' to='/signup' gray={true} />
+                </div>
+            )}
+        </header>
+    );
 };
 
 export default Header;
