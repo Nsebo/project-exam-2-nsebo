1066ebd68fc5e537a668850e140e4048f5b1be37
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-31 11:28:52 +0200

    Update Login.jsx

--- src/authentication/Login/Login.jsx
+++ src/authentication/Login/Login.jsx
@@ -19,75 +19,76 @@ const Login = () => {
   } = useContext(LoginContext);
   const { signUpData } = useContext(AuthContext);
   return (
-      <main>
-        <div className={styles.login_container}>
-          <div className={styles.login_image}>
-            <img src={loginImg} alt='login Image' />
+    <main>
+      <div className={styles.login_container}>
+        <div className={styles.login_image}>
+          <img src={loginImg} alt='login Image' />
+        </div>
+        <div className={styles.login_details}>
+          <Link to='/'>&rarr;</Link>
+          <div className={styles.login_details_heading}>
+            <h1 className={styles.login_title}>
+              Welcome back, {signUpData.name}
+            </h1>
+            <p className={styles.signup_title}>
+              Welcome back! Please enter your details
+            </p>
           </div>
-          <div className={styles.login_details}>
-            <div className={styles.login_details_heading}>
-              <h1 className={styles.login_title}>
-                Welcome back, {signUpData.name}
-              </h1>
-              <p className={styles.signup_title}>
-                Welcome back! Please enter your details
+          <form className={styles.form} onSubmit={handleLoginSubmit}>
+            <div className={styles.form_body}>
+              <div className={styles.email}>
+                <label className='form__label' htmlFor='email'>
+                  Email{' '}
+                </label>
+                <input
+                  type='email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className={styles.form__input}
+                  placeholder='Enter email'
+                />
+              </div>
+              <div className='password'>
+                <label className='form__label' htmlFor='password'>
+                  Password{' '}
+                </label>
+                <input
+                  type='password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className={styles.form__input}
+                  placeholder='Enter Password'
+                  required
+                />
+              </div>
+              <div className={styles.forgot_password}>
+                <p className=''>Remember for 30</p>
+                <Link className=''>forget password?</Link>
+              </div>
+            </div>
+            {errorMessage && <p>{errorMessage}</p>}
+            <div className={styles.btn}>
+              <button type='submit' className={styles.submit_btn}>
+                Log in
+              </button>
+            </div>
+            <div className={styles.btn}>
+              <button type='submit' className={styles.google_btn}>
+                <img src={googleIcon} alt='google Icon' />
+                Log in with google
+              </button>
+            </div>
+            <div>
+              <p className={styles.signup}>
+                Don’t have an account?{' '}
+                <Link to='/signup'> Sign up for free</Link>
               </p>
             </div>
-            <form className={styles.form} onSubmit={handleLoginSubmit}>
-              <div className={styles.form_body}>
-                <div className={styles.email}>
-                  <label className='form__label' htmlFor='email'>
-                    Email{' '}
-                  </label>
-                  <input
-                      type='email'
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      required
-                      className={styles.form__input}
-                      placeholder='Enter email'
-                  />
-                </div>
-                <div className='password'>
-                  <label className='form__label' htmlFor='password'>
-                    Password{' '}
-                  </label>
-                  <input
-                      type='password'
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      className={styles.form__input}
-                      placeholder='Enter Password'
-                      required
-                  />
-                </div>
-                <div className={styles.forgot_password}>
-                  <p className=''>Remember for 30</p>
-                  <Link className=''>forget password?</Link>
-                </div>
-              </div>
-              {errorMessage && <p>{errorMessage}</p>}
-              <div className={styles.btn}>
-                <button type='submit' className={styles.submit_btn}>
-                  Log in
-                </button>
-              </div>
-              <div className={styles.btn}>
-                <button type='submit' className={styles.google_btn}>
-                  <img src={googleIcon} alt='google Icon' />
-                  Log in with google
-                </button>
-              </div>
-              <div>
-                <p className={styles.signup}>
-                  Don’t have an account?{' '}
-                  <Link to='/signup'> Sign up for free</Link>
-                </p>
-              </div>
-            </form>
-          </div>
+          </form>
         </div>
-      </main>
+      </div>
+    </main>
   );
 };
 
