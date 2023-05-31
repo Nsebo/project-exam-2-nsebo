1888d3debbffbd88b731eaf1b42579048cdfa6ca
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-31 11:37:32 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { VenueProvider } from './context/venue-context.jsx';
 import { AuthProvider } from './context/auth-context.jsx';
 import { LoginProvider } from './context/login-context.jsx';
+import { BookingProvider } from './context/bookings-context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <LoginProvider>
-                    <VenueProvider>
-                        <App />
-                    </VenueProvider>
-                </LoginProvider>
-            </AuthProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <LoginProvider>
+          <VenueProvider>
+            <BookingProvider>
+              <App />
+            </BookingProvider>
+          </VenueProvider>
+        </LoginProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
