767ebc81199c9b8e4b1b6a8a4c11fdb9d739d62e
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-31 11:36:29 +0200

    Update Venue.jsx

--- src/components/Venue/Venue.jsx
+++ src/components/Venue/Venue.jsx
@@ -4,27 +4,27 @@ import { VenueContext } from '../../context/venue-context';
 import VenueCard from '../VenueCard/VenueCard';
 
 const Venue = () => {
-    const { venues } = useContext(VenueContext);
-    const sixVenues = venues.slice(0, 6);
+  const { venues } = useContext(VenueContext);
+  const sixVenues = venues.slice(0, 6);
 
-    return (
-        <section className={styles.venue_wrapper}>
-            <h3>TOP DESTINATION</h3>
-            <h2>Explore Top Destination</h2>
-            <div className={styles.venue_container}>
-                {sixVenues.map((venue) => {
-                    return (
-                        <VenueCard
-                            imgUrl={venue.media[0]}
-                            price={venue.price}
-                            name={venue.name}
-                            id={venue.id}
-                        />
-                    );
-                })}
-            </div>
-        </section>
-    );
+  return (
+    <section className={styles.venue_wrapper}>
+      <h3>TOP DESTINATION</h3>
+      <h2>Explore Top Destination</h2>
+      <div className={styles.venue_container}>
+        {sixVenues.map((venue) => {
+          return (
+            <VenueCard
+              imgUrl={venue.media[0]}
+              price={venue.price}
+              name={venue.name}
+              id={venue.id}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Venue;
