fa1f7b21aca7f5ec09cbebe62200f784d7eca3bb
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-31 11:28:32 +0200

    Update Signup.jsx

--- src/authentication/Signup/Signup.jsx
+++ src/authentication/Signup/Signup.jsx
@@ -26,121 +26,121 @@ const Signup = () => {
   } = useContext(AuthContext);
 
   return (
-      <main>
-        <div className={styles.signup_container}>
-          <div className={styles.signup_image}>
-            <img src={signupImg} alt='Signup Image' />
+    <main>
+      <div className={styles.signup_container}>
+        <div className={styles.signup_image}>
+          <img src={signupImg} alt='Signup Image' />
+        </div>
+        <div className={styles.signup_details}>
+          <div className={styles.signup_details_heading}>
+            <h1 className={styles.signup_title}>Create an Account</h1>
+            <p className={styles.signup_para}>
+              Became holidaze member, you’ll get exclusive offers from holidaze
+            </p>
           </div>
-          <div className={styles.signup_details}>
-            <div className={styles.signup_details_heading}>
-              <h1 className={styles.signup_title}>Create an Account</h1>
-              <p className={styles.signup_para}>
-                Became holidaze member, you’ll get exclusive offers from holidaze
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <div className={styles.form_body}>
+              <div className={styles.username}>
+                <label className='form__label' htmlFor='name'>
+                  Name:
+                </label>
+                <input
+                  className={styles.form_input}
+                  type='text'
+                  id='name'
+                  placeholder='Enter your name'
+                  name='name'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className={styles.email}>
+                <label className={styles.form_label} htmlFor='email'>
+                  Email:
+                </label>
+                <input
+                  type='email'
+                  id='email'
+                  className={styles.form_input}
+                  placeholder='Enter email'
+                  name='email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className={styles.password}>
+                <label className={styles.form_label} htmlFor='password'>
+                  Password:
+                </label>
+                <input
+                  className={styles.form_input}
+                  type='password'
+                  id='password'
+                  placeholder='Create a Password'
+                  name='password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className={styles.confirm_password}>
+                <label className={styles.form_label} htmlFor='confirmPassword'>
+                  Confirm Password:
+                </label>
+                <input
+                  className={styles.form_input}
+                  type='password'
+                  id='confirmPassword'
+                  placeholder='Confirm Password'
+                  name='confirmPassword'
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+              <div className={styles.Avatar}>
+                <label className={styles.form_label} htmlFor='avatar'>
+                  Avatar:
+                </label>
+                <input
+                  type='text'
+                  name='avatarUrl'
+                  id='avatar'
+                  className={styles.form_input}
+                  placeholder='Avatar URL'
+                  value={avatar}
+                  onChange={(e) => setAvatar(e.target.value)}
+                />
+              </div>
+              <div className={styles.venueManager}>
+                <label className={styles.form_label} htmlFor='venueManager'>
+                  Venue Manager:
+                  <input
+                    type='checkbox'
+                    checked={venueManager}
+                    onChange={(e) => setVenueManager(e.target.checked)}
+                  />
+                </label>
+              </div>
+            </div>
+            {errorMessage && <p>{errorMessage}</p>}
+            <div className={styles.btn}>
+              <button type='submit' className={styles.submit_btn}>
+                Create an Account
+              </button>
+              <button type='submit' className={styles.google_btn}>
+                <img src={googleIcon} alt='Google Icon' />
+                Sign up with Google
+              </button>
+            </div>
+
+            <div>
+              <p className={styles.signup}>
+                Already have an account? <Link to='/login'>Log in</Link>
               </p>
             </div>
-            <form className={styles.form} onSubmit={handleSubmit}>
-              <div className={styles.form_body}>
-                <div className={styles.username}>
-                  <label className='form__label' htmlFor='name'>
-                    Name:
-                  </label>
-                  <input
-                      className={styles.form_input}
-                      type='text'
-                      id='name'
-                      placeholder='Enter your name'
-                      name='name'
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
-                <div className={styles.email}>
-                  <label className={styles.form_label} htmlFor='email'>
-                    Email:
-                  </label>
-                  <input
-                      type='email'
-                      id='email'
-                      className={styles.form_input}
-                      placeholder='Enter email'
-                      name='email'
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className={styles.password}>
-                  <label className={styles.form_label} htmlFor='password'>
-                    Password:
-                  </label>
-                  <input
-                      className={styles.form_input}
-                      type='password'
-                      id='password'
-                      placeholder='Create a Password'
-                      name='password'
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                <div className={styles.confirm_password}>
-                  <label className={styles.form_label} htmlFor='confirmPassword'>
-                    Confirm Password:
-                  </label>
-                  <input
-                      className={styles.form_input}
-                      type='password'
-                      id='confirmPassword'
-                      placeholder='Confirm Password'
-                      name='confirmPassword'
-                      value={confirmPassword}
-                      onChange={(e) => setConfirmPassword(e.target.value)}
-                  />
-                </div>
-                <div className={styles.Avatar}>
-                  <label className={styles.form_label} htmlFor='avatar'>
-                    Avatar:
-                  </label>
-                  <input
-                      type='text'
-                      name='avatarUrl'
-                      id='avatar'
-                      className={styles.form_input}
-                      placeholder='Avatar URL'
-                      value={avatar}
-                      onChange={(e) => setAvatar(e.target.value)}
-                  />
-                </div>
-                <div className={styles.venueManager}>
-                  <label className={styles.form_label} htmlFor='venueManager'>
-                    Venue Manager:
-                    <input
-                        type='checkbox'
-                        checked={venueManager}
-                        onChange={(e) => setVenueManager(e.target.checked)}
-                    />
-                  </label>
-                </div>
-              </div>
-              {errorMessage && <p>{errorMessage}</p>}
-              <div className={styles.btn}>
-                <button type='submit' className={styles.submit_btn}>
-                  Create an Account
-                </button>
-                <button type='submit' className={styles.google_btn}>
-                  <img src={googleIcon} alt='Google Icon' />
-                  Sign up with Google
-                </button>
-              </div>
-
-              <div>
-                <p className={styles.signup}>
-                  Already have an account? <Link to='/login'>Log in</Link>
-                </p>
-              </div>
-            </form>
-          </div>
+          </form>
         </div>
-      </main>
+      </div>
+    </main>
   );
 };
 
