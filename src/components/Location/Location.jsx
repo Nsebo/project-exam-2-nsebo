b257ae239e96ea0015fe0657e67419c5d293e05e
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-31 11:36:22 +0200

    Update Location.jsx

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -2,22 +2,22 @@ import styles from './Location.module.scss';
 import searchIcon from '../../assets/search.svg';
 
 const Location = () => {
-    return (
-        <form className={styles.heroBanner}>
-            <div className={styles.search}>
+  return (
+    <form className={styles.heroBanner}>
+      <div className={styles.search}>
         <span>
           <img src={searchIcon} alt='search icon' />
         </span>
-                <input
-                    type='search'
-                    name='text'
-                    className={styles.search}
-                    id='text'
-                    placeholder='Search'
-                />
-            </div>
-        </form>
-    );
+        <input
+          type='search'
+          name='text'
+          className={styles.search}
+          id='text'
+          placeholder='Search'
+        />
+      </div>
+    </form>
+  );
 };
 
 export default Location;
