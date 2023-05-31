eb4e00631079463f4d5500d99e2442325a13be7a
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-31 11:33:30 +0200

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,13 +5,13 @@ import Button from '../../layouts/Button/Button';
 
 const Hero = () => {
   return (
-    <section className={styles.hero_container}>
-      <div className={styles.hero_details}>
-        <h1>STAY WITH COMFORT</h1>
-        <p>Stay in the world's most remarkable homes</p>
-        <Button buttonText='Explore more' to='/venues' white={true}/>
-      </div>
-    </section>
+      <section className={styles.hero_container}>
+          <div className={styles.hero_details}>
+              <h1>STAY WITH COMFORT</h1>
+              <p>Stay in the World's most remarkable homes</p>
+              <Button buttonText='Explore more' to='/venues' white={true}/>
+          </div>
+      </section>
   );
 };
 
