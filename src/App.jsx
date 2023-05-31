4fa3fe95065b9a6dba111eb3ffe1b5bdd84b4aa1
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-05-31 11:29:03 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,26 @@ import Home from './pages/Home/Home.jsx';
 import Venues from './pages/Venues/Venues.jsx';
 import Venue from './pages/Venue/Venue.jsx';
 import CreateVenue from './components/CreateVenue/CreateVenue.jsx';
+import Text from './text.jsx';
 import VenueManager from './pages/VenueManager/VenueManager.jsx';
+import Bookings from './components/Bookings/Bookings.jsx';
 
 function App() {
-    return (
-        <div className='app'>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='signup' element={<Signup />} />
-                <Route path='login' element={<Login />} />
-                <Route path='/venues' element={<Venues />} />
-                <Route path='/my-venues' element={<VenueManager />} />
-                <Route path='/create-venues' element={<CreateVenue />} />
-                <Route path='/venue/:venueId' element={<Venue />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='login' element={<Login />} />
+        <Route path='/venues' element={<Venues />} />
+        <Route path='/text' element={<Text />} />
+        <Route path='/my-venues' element={<VenueManager />} />
+        <Route path='/my-bookings' element={<Bookings />} />
+        <Route path='/create-venues' element={<CreateVenue />} />
+        <Route path='/venue/:venueId' element={<Venue />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
