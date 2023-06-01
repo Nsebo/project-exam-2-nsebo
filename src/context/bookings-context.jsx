c62817b3b49b0d8fe0e7e0721ab847f20a739add
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-06-01 12:31:54 +0200

    Update bookings-context.jsx

--- src/context/bookings-context.jsx
+++ src/context/bookings-context.jsx
@@ -3,18 +3,13 @@ import { createContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export const BookingContext = createContext(null);
-
 export const BookingProvider = ({ children }) => {
   const [venue, setVenue] = useState('');
-
   const { venueId } = useParams();
-
   const [dateFrom, setDateFrom] = useState('');
   const [dateTo, setDateTo] = useState('');
   const [guests, setGuests] = useState(1);
-  // const [venueId, setVenueId] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-
   const {
     name,
     description,
@@ -27,28 +22,20 @@ export const BookingProvider = ({ children }) => {
     location,
     maxGuests,
   } = venue;
-
-  console.log(venueId);
-
   const handleBookingSubmit = async (e) => {
     e.preventDefault();
-
     if (isNaN(guests)) {
       setErrorMessage('Please enter a valid number for guests.');
       return;
     }
-
     const bookingData = {
       dateFrom: new Date(dateFrom),
       dateTo: new Date(dateTo),
       guests: parseInt(guests),
       venueId,
     };
-    console.log(dateFrom, dateTo, guests, venueId);
-
     try {
       const accessToken = localStorage.getItem('accessToken');
-
       const response = await axios.post(
         'https://nf-api.onrender.com/api/v1/holidaze/bookings',
         bookingData,
@@ -58,32 +45,11 @@ export const BookingProvider = ({ children }) => {
           },
         }
       );
-
-      console.log('Booking created:', response.data);
-      // You can handle the success response here, such as showing a success message or redirecting the user.
     } catch (error) {
       console.error('Booking creation failed:', error);
       setErrorMessage('Failed to create booking. Please try again.');
-      // You can handle the error response here, such as showing an error message to the user.
     }
   };
-
-  // console.log(errorMessage, venueId);
-
-  // console.log(errorMessage);
-
-  // const isoCreatedDateString = created;
-  // const isoUpdatedDateString = updated;
-  // const createdDate = new Date(isoCreatedDateString);
-  // const updatedDate = new Date(isoUpdatedDateString);
-
-  // const options = { day: '2-digit', month: 'short', year: 'numeric' };
-  // const formatter = new Intl.DateTimeFormat('en-US', options);
-  // const formattedCreatedDate = formatter.format(createdDate);
-  // const formattedUpdatedDate = formatter.format(updatedDate);
-  // console.log(formattedCreatedDate);
-  // console.log(formattedUpdatedDate);
-
   useEffect(() => {
     axios
       .get(`https://nf-api.onrender.com/api/v1/holidaze/venues/${venueId}`)
@@ -94,9 +60,6 @@ export const BookingProvider = ({ children }) => {
         console.log(err);
       });
   }, []);
-
-  console.log(venue, venueId);
-
   return (
     <BookingContext.Provider
       value={{
